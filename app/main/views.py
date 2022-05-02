37e4735350074869f6f891fd23ff63e563cc6125
Willbeckh <wileyhndirangu04@gmail.com>
2022-05-02 16:15:03 +0300

    ft: create variable for getting news type

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,14 @@
 from . import main
+from flask import render_template
+from ..request import get_news_sources
 
 # first default route
 @main.route('/')
 def index():
     """
-    Landing page route
+    View root page endpoint that returns the landing page and its data
     """
-    return "Yoow Blueprints!"
+    # return "Yoow Blueprints!"
+    # getting top headlines news
+    top_news = get_news_sources('top-headlines')
+    return render_template('index.html', trending=top_news)
