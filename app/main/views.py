0d36627328c6485c2c5583287fc60a0d814318a0
Willbeckh <wileyhndirangu04@gmail.com>
2022-05-04 19:07:00 +0300

    fx: add articles reference to return statement

--- app/main/views.py
+++ app/main/views.py
@@ -1,14 +1,25 @@
 from . import main
-from flask import render_template
-from ..request import get_news_sources
+from flask import render_template, url_for
+from ..request import get_news_sources, get_articles
 
 # first default route
+
+
 @main.route('/')
 def index():
     """
     View root page endpoint that returns the landing page and its data
     """
-    # return "Yoow Blueprints!"
-    # getting top headlines news
-    top_news = get_news_sources('top-headlines')
-    return render_template('index.html', trending=top_news)
+    source_domain = 'techcrunch.com'  # TODO: make the value dynamic
+    # TODO: make the value dynamic 'technology' is = user_input/choice
+    top_news = get_news_sources('technology')
+    articles = get_articles(source_domain)
+    return render_template('index.html', trending=top_news, articles=articles)
+
+
+# TODO: create list that stores all the news sources
+# *LOOP through the list and access its value
+#  *IF checked sources, RETURN all articles using selected sources.
+
+# TODO: article details page.
+# *User Interface, design...
