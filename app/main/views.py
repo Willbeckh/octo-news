28549e7d8f014313f5fe1684c11f57cbeb369f18
Willbeckh <wileyhndirangu04@gmail.com>
2022-05-05 14:13:22 +0300

    ft: update form data to list for getting multiple news sources

--- app/main/views.py
+++ app/main/views.py
@@ -2,24 +2,27 @@ from . import main
 from flask import render_template, url_for, request
 from ..request import get_news_category, get_articles
 
+
 # first default route
-
-
 @main.route('/', methods=['GET','POST'])
 def index():
     """
     View root page endpoint that returns the landing page and its data
     """
     news_category = get_news_category('general') # default sources
-    articles_source = get_articles('bbc-news')
+    articles_source = get_articles('bbc-news, abc-news, the-verge')
     
     try:
         if request.method == 'POST':
             user_category_pick = request.form.get('categories')
             category = get_news_category(user_category_pick)
-            user_source_choice = request.form.get('source-tag')
-            source_id = get_articles(user_source_choice)
-            return render_template('index.html', trending=category, articles=source_id)  
+            
+            user_source_choice = request.form.getlist('source-tag')
+            source_list = ' ,'.join([str(word) for word in user_source_choice]) #convert list to comma separated strings.
+            source_ids = get_articles(source_list)
+            
+            return render_template('index.html', trending=category, articles=source_ids)
+          
     except TypeError: # incase the news source picked doesn't have the selected category
         return render_template('404.html')
     except KeyError: 
